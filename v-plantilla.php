ab1928f274198edbabfe1f1bcb18a4f36247b8db
Javerson287 <81368864+Javerson287@users.noreply.github.com>
2021-05-21 10:30:32 -0500

    actualizacion para chorome

--- v-plantilla.php
+++ v-plantilla.php
@@ -1,3 +1,4 @@
+  
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,6 +7,8 @@
         <meta charset="utf-8">
         
         <title>Título del sitio.</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <link rel="stylesheet" type="text/css" href="css/estilo.css">
 
